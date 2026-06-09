dfeabb9a089dc0adfb48a6fb62c188a6bb778d7f
qp-1130260 <gustavo.brauza@queropassagem.com.br>
2026-06-09 12:50:09 -0300

    Tratamento de erro de json encode

--- app/Console/Commands/ExportViacoes.php
+++ app/Console/Commands/ExportViacoes.php
@@ -1,12 +1,13 @@
 <?php
+
 namespace App\Console\Commands;
 
+use App\Services\ViacaoService;
 use Illuminate\Console\Attributes\Description;
 use Illuminate\Console\Attributes\Signature;
 use Illuminate\Console\Command;
-use App\Services\ViacaoService;
 
-#[Signature('viacoes:export {filename : Digite o nome do arquivo JSON de destino}')] //o # tem a mesma valia do declarado no import
+#[Signature('viacoes:export {filename : Digite o nome do arquivo JSON de destino}')] // o # tem a mesma valia do declarado no import
 #[Description('Exporta as TODAS as viações,ativas e inativas,excluídas não...')]
 class ExportViacoes extends Command
 {
@@ -21,7 +22,7 @@ class ExportViacoes extends Command
         $filename = $this->argument('filename');
 
         // caso o usuário não digite .json, é adicionado
-        if (!str_ends_with($filename, '.json')) {
+        if (! str_ends_with($filename, '.json')) {
             $filename .= '.json';
         }
         try {
@@ -29,25 +30,34 @@ class ExportViacoes extends Command
             $viacoes = $this->viacaoService->exportViacoes();
 
             if ($viacoes->isEmpty()) {
-                $this->warn("Não achei nada para exportar"); //warn é apenas um tipo de string exclusivo pra avisos ou warnings
+                $this->warn('Não achei nada para exportar'); // warn é apenas um tipo de string exclusivo pra avisos ou warnings
+
                 return self::SUCCESS;
             }
 
             // manipula o json armazenado em $viacoes(doService),usando as flags/constantes JSON_PRETTY_PINT e JSON_UNESCAPEWD_UNICODE
             $jsonText = json_encode($viacoes, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
 
+            //bloco if que se o json der falha(false) ele ja manda pro demonio
+            if ($jsonText === false) {
+                $this->error('Algo deu zika');
+                $this->error(json_last_error_msg());
+                return self::FAILURE;
+            }
+
             $rightPath = storage_path("app/{$filename}");
 
             // cria o arquivo na máquina e injeta o json
             file_put_contents($rightPath, $jsonText);
 
-            $this->info("deu boa!");
+            $this->info('deu boa!');
 
             return self::SUCCESS;
         } catch (\Exception $exception) {
 
-            $this->error('Algo deu errado'); //normalmente se dar certo ele retorna 0,se errado 1
+            $this->error('Algo deu errado'); // normalmente se dar certo ele retorna 0,se errado 1
             $this->error($exception->getMessage());
+
             return self::FAILURE;
         }
     }
