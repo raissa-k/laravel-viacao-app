6d12ee59a95784c1c1b6a93955a1af429254f768
Caio <caio.marcidele@queropassagem.com.br>
2026-06-11 13:43:19 -0300

    H2 alterado no search-bar.blade.php

--- resources/views/components/search-bar.blade.php
+++ resources/views/components/search-bar.blade.php
@@ -1,8 +1,9 @@
 @props(['layout' => 'vertical'])
 
 <div class="card search-card-{{ $layout }}">
-    <h2 class="card-title" style="margin-bottom: 15px; font-size: 1.1rem;">Buscar passagem</h2>
-
+    @if($layout === 'vertical')
+        <h2 class="card-title">Buscar passagem</h2>
+    @endif
     <form class="search-form {{ $layout === 'horizontal' ? 'search-form-horizontal' : '' }}" action="{{ route('busca') }}" method="GET">
 
         <div class="field">
