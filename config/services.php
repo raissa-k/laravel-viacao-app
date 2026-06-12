98a59168d483349c2a7052e67572d119d5064648
BrunoBuff <bruno.bff.farias@gmail.com>
2026-06-12 13:48:31 -0300

    Comando para padronização

--- config/services.php
+++ config/services.php
@@ -16,21 +16,21 @@ return [
     |
     */
 
-    'postmark' => [
+    'postmark'       => [
         'key' => env('POSTMARK_API_KEY'),
     ],
 
-    'resend'   => [
+    'resend'         => [
         'key' => env('RESEND_API_KEY'),
     ],
 
-    'ses'      => [
+    'ses'            => [
         'key'    => env('AWS_ACCESS_KEY_ID'),
         'secret' => env('AWS_SECRET_ACCESS_KEY'),
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
 
-    'slack'    => [
+    'slack'          => [
         'notifications' => [
             'bot_user_oauth_token' => env('SLACK_BOT_USER_OAUTH_TOKEN'),
             'channel'              => env('SLACK_BOT_USER_DEFAULT_CHANNEL'),
@@ -38,7 +38,7 @@ return [
     ],
 
     'transporte_api' => [
-        'url' => env('TRANSPORTE_API_URL'),
+        'url'   => env('TRANSPORTE_API_URL'),
         'token' => env('TRANSPORTE_API_TOKEN'),
     ]
 
