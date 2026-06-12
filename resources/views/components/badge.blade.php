4329dd5995774122ee7a8663fdc75112b070992f
qp-1130260 <gustavo.brauza@queropassagem.com.br>
2026-06-12 09:20:01 -0300

    ajustes de issues demandadas

--- resources/views/components/badge.blade.php
+++ resources/views/components/badge.blade.php
@@ -1,8 +1,8 @@
-@props(['rotulo', 'tipo' => 'convencional'])
+@props(['rotulo', 'tipo' => 'padrao'])
 {{--se alguem passar um tipo diferente do que ja tem,ele manda um convencional mesmo,nao quebra--}}
 
 @php
-    $tiposConhecidos = ['convencional', 'executivo', 'semi-leito', 'leito', 'success', 'error', 'info', 'warning'];
+    $tiposConhecidos = ['convencional', 'executivo', 'semi-leito', 'leito', 'success', 'error', 'info', 'warning','padrao'];
     $tipoLimpo = strtolower($tipo);
     $classeTipo = in_array($tipoLimpo, $tiposConhecidos) ? $tipoLimpo : 'convencional';
 @endphp
